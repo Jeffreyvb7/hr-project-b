9394bec1576dd876c43fa6422991a4118a2cc2cf
jasperschinkel <jasper-schinkel1999@hotmail.com>
2020-04-13 22:29:31 +0200

    kleine dingetjes toegevoegd

--- Infomation.cs
+++ Infomation.cs
@@ -8,8 +8,8 @@ namespace Applicatie
     {
         public static void showinfo(){
             // hier komen alle details over ons bedrijf
-            Console.WriteLine(
-            "Alle informatie over het bedrijf: \n\n" +
+            Console.WriteLine("====================" +
+            "\nAlle informatie over het bedrijf: \n\n" +
             "420 Inch Spacebarz\n" +
             "We make Escape Rooms for everyone who enjoys puzzling and problem solving!\n\n" + 
             "if you have a question, you can always contact us through:\n" +
@@ -17,6 +17,7 @@ namespace Applicatie
             "Facebook: 420 Inch Spacebarz\n" +
             "E-mail: 420inchspacebarz@hotmail.com\n" +
             "Phone number: 0612345678\n\n" +
+            "====================\n" +
             "Please press enter to return to the menu"
             );
         }
