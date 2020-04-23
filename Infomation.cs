e6f8dd38547669d4a07ae3b498d9702c0ecf02db
jasperschinkel <jasper-schinkel1999@hotmail.com>
2020-04-23 14:22:57 +0200

    code convention change

--- Infomation.cs
+++ Infomation.cs
@@ -6,9 +6,10 @@ namespace Applicatie
 {
     class Infomation
     {
-        public static void showinfo(){
+        public static void showinfo()
+        {
             // hier komen alle details over ons bedrijf
-            Console.WriteLine("====================" +
+            Console.WriteLine("======================================" +
             "\nAlle informatie over het bedrijf: \n\n" +
             "420 Inch Spacebarz\n" +
             "We make Escape Rooms for everyone who enjoys puzzling and problem solving!\n\n" + 
@@ -16,8 +17,9 @@ namespace Applicatie
             "Twitter: @420inchspacebarz\n" +
             "Facebook: 420 Inch Spacebarz\n" +
             "E-mail: 420inchspacebarz@hotmail.com\n" +
-            "Phone number: 0612345678\n\n" +
-            "====================\n" +
+            "Phone number: 0612345678\n" +
+            "Location: Wijnhaven 107  3011 WN\n" +
+            "======================================\n" +
             "Please press enter to return to the menu"
             );
         }
