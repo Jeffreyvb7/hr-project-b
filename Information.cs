d481405e0a59a8b790b76eafc54519ca5ee4e827
Jeffrey van Bennekom <jeffreyvb7@gmail.com>
2020-05-18 23:12:33 +0200

    Added the function to edit the infromation panel

--- Information.cs
+++ Information.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Applicatie
 {
     class Information
     {
+        public string companyName { get; set; }
+        public string companyInfo { get; set; }
+        public string twitterInfo { get; set; }
+        public string facebookInfo { get; set; }
+        public string emailInfo { get; set; }
+        public string phoneNum { get; set; }
+        public string location { get; set; }
+
         public static void showinfo()
         {
-            var information = JsonManager.GetData<InformationItems>("InfoPage", "information");
+            var information = JsonManager.GetData<Information>("InfoPage", "information");
 
             // hier wordt alles geprint naar console
             Console.WriteLine("======================================" +
@@ -25,17 +34,83 @@ namespace Applicatie
             );
         }
 
-    }
+        public static void EditInfo()
+        {
+            var information = JsonManager.GetData<Information>("InfoPage", "information");
+            Console.Clear();
+            Console.Write(
+                "Choose what you want to edit:\n" +
+                "\t1 - Company Name\n" +
+                "\t2 - Company Info\n" +
+                "\t3 - Twitter\n" +
+                "\t4 - Facebook\n" +
+                "\t5 - Email\n" +
+                "\t6 - Phone Number\n" +
+                "\t7 - Location\n" +
+                "\t= - Cancel\n" +
+                "\nPlease choose one of the following options:\n"
+            );
 
-    class InformationItems
-    {
-        public string companyName { get; set; }
-        public string companyInfo { get; set; }
-        public string twitterInfo { get; set; }
-        public string facebookInfo { get; set; }
-        public string emailInfo { get; set; }
-        public string phoneNum { get; set; }
-        public string location { get; set; }
+            while(true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        EditInfoJson(information, "companyName");
+                        break;
+                    case "2":
+                        EditInfoJson(information, "companyInfo");
+                        break;
+                    case "3":
+                        EditInfoJson(information, "twitterInfo");
+                        break;
+                    case "4":
+                        EditInfoJson(information, "facebookInfo");
+                        break;
+                    case "5":
+                        EditInfoJson(information, "emailInfo");
+                        break;
+                    case "6":
+                        EditInfoJson(information, "phoneNum");
+                        break;
+                    case "7":
+                        EditInfoJson(information, "location");
+                        break;
+                    case "=":
+                        return;
+                    default:
+                        Console.WriteLine("Please enter a valid option");
+                        break;
+                }
+            }
+        }
+
+        private static void EditInfoJson(Information information, string property)
+        {
+            Console.Clear();
+            var currentValue = information.GetType().GetProperty(property).GetValue(information);
+
+            Console.Write(
+                $"Editing {property}, currently: {currentValue}\n" +
+                $"Please enter new {property}: \n"
+            );
+
+            while(true)
+            {
+                string value = Console.ReadLine();
+                if(value.Any())
+                {
+                    information.GetType().GetProperty(property).SetValue(information, value, null);
+                    Console.Clear();
+                    Console.WriteLine($"New {property}: {value}");
+                    JsonManager.SaveData<Information>(information, "InfoPage", "information");
+                    return;
+                } else {
+                    Console.WriteLine("You need to enter something");
+                }
+            }
+
+        }
 
     }
 }
