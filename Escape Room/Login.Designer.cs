8959ce2d02211e43d7f04a05728d0b1b2e61bba9
Jeffrey van Bennekom <jeffreyvb7@gmail.com>
2020-06-02 11:26:33 +0200

    Made WindowsForms

---
+++ Escape Room/Login.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Escape_Room
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PassWordBox = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(366, 277);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(75, 23);
+            this.RegisterButton.TabIndex = 0;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(366, 226);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 1;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Location = new System.Drawing.Point(301, 121);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(211, 20);
+            this.UserNameBox.TabIndex = 2;
+            // 
+            // PassWordBox
+            // 
+            this.PassWordBox.Location = new System.Drawing.Point(301, 170);
+            this.PassWordBox.Name = "PassWordBox";
+            this.PassWordBox.PasswordChar = '*';
+            this.PassWordBox.Size = new System.Drawing.Size(211, 20);
+            this.PassWordBox.TabIndex = 3;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblError.Location = new System.Drawing.Point(298, 193);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.PassWordBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.RegisterButton);
+            this.Name = "Login";
+            this.Text = "LoginScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PassWordBox;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
